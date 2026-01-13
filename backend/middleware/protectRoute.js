2b549d9cf99428bdad00dbac5c4712be8556a49d
Byamkesh <kaiwartyabyamkesh7@gmail.com>
2026-01-13 18:44:16 +0530

    protectroute added in middleware to get rid from unauthorized access

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,20 +1,32 @@
-// import jwt from "jsonwebtoken";
-// import { User } from "../models/user.model.js";
-// import { ENV_VARS } from "../config/envVars.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import { ENV_VARS } from "../config/envVars.js";
 
-// export const protectRoute = async (req, res, next) => {
-//   try {
-//     const token = req.cookies["jwt-cinepulse"];
-//     if (!token) {
-//       return res
-//         .status(401)
-//         .json({ success: false, message: "Unauthorized: No token provided" });
-//     }
-//     const decoded = jwt.verify(token, ENV_VARS.JWT_SECRET);
-//     if (!decoded) {
-//       return res
-//         .status(401)
-//         .json({ success: false, message: "Unauthorized: Invalid token" });
-//     }
-//   } catch (error) {}
-// };
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies["jwt-cinepulse"];
+    if (!token) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized: No token provided" });
+    }
+    const decoded = jwt.verify(token, ENV_VARS.JWT_SECRET);
+    if (!decoded) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized: Invalid token" });
+    }
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Unauthorized: User not found" });
+    }
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.error("Error in protectRoute middleware: ", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
