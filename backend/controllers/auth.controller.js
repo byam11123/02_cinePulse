3a459878a714a6ec96ce4ad9b106397a1af2f486
Byamkesh <kaiwartyabyamkesh7@gmail.com>
2026-01-13 15:38:23 +0530

    logout and login route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -67,9 +67,47 @@ export async function signup(req, res) {
 }
 
 export async function login(req, res) {
-  res.send("Login route");
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+    generateTokenAndSetCookie(user._id, res);
+    res.status(200).json({
+      success: true,
+      user: {
+        ...user._doc,
+        password: "", // Don't send password in response
+      },
+    });
+  } catch (error) {
+    console.error("Error in login controller: ", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 }
 
 export async function logout(req, res) {
-  res.send("Logout route");
+  try {
+    res.clearCookie("jwt-cinepulse");
+    res.status(200).json({ success: true, message: "Logged out successfully" });
+  } catch (error) {
+    console.error("Error in logout controller: ", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 }
