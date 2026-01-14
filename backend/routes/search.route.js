9164857dbd26dcf477fce67994eb508beb465a52
byam11123 <kaiwartyabyamkesh7@gmail.com>
2026-01-14 12:08:14 +0530

    added more feature about search route and complete the mvp backend

--- backend/routes/search.route.js
+++ backend/routes/search.route.js
@@ -3,11 +3,17 @@ import {
   searchPerson,
   searchMovie,
   searchTv,
+  removeItemFromSearchHistory,
+  getSearchHistory,
 } from "../controllers/search.controller.js";
+import { get } from "mongoose";
 const router = express.Router();
 
 router.get("/person/:query", searchPerson);
 router.get("/movie/:query", searchMovie);
 router.get("/tv/:query", searchTv);
+router.get("/history", getSearchHistory);
+
+router.delete("/history/:id", removeItemFromSearchHistory);
 
 export default router;
